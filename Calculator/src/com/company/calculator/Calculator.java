132d766ba3495f50c1d732bf303f91268daf0d9d
harshali444 <jadhav.h0564@gmail.com>
2020-10-28 22:11:53 +0530

    add calculator file

---
+++ Calculator/src/com/company/calculator/Calculator.java
@@ -0,0 +1,22 @@
+package com.company.calculator;
+
+import java.util.Scanner;
+
+public class Calculator {
+	
+	public static void main(String[] args) {
+		
+		Scanner myObj = new Scanner(System.in);
+		byte a = myObj.nextByte();
+		byte b = myObj.nextByte();	
+		
+		int c = a + b;
+		
+		System.out.println("addition is : " + c);
+		
+		
+		
+		
+	}
+
+}
